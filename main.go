5d2e62a60fa4bcde1c6cd60806206f4bbd88d41a
AMD PC <shitga@gmail.com>
2024-02-07 11:25:50 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/supersida159/e-commerce/pkg/app_context"
+	"github.com/supersida159/e-commerce/pkg/config"
+	dbs "github.com/supersida159/e-commerce/pkg/db"
+	"github.com/supersida159/e-commerce/pkg/pubsub/pubsublocal"
+	"github.com/supersida159/e-commerce/pkg/redis"
+	httpServer "github.com/supersida159/e-commerce/src/server"
+	"github.com/supersida159/e-commerce/src/users/entities"
+	"github.com/supersida159/e-commerce/src/users/repository_user"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	// logger.Initialize(cfg.Environment)
+
+	db, err := dbs.NewDatabase(cfg.DatabaseURI)
+	fmt.Println("url db:", cfg.DatabaseURI)
+	if err != nil {
+		logrus.Fatal("Cannot connect to database", err)
+	}
+	err = db.AutoMigrate(&entities.User{})
+	cache := redis.NewRedis(redis.Config{
+		Address:  cfg.RedisURI,
+		Password: cfg.RedisPassword,
+		Database: cfg.RedisDB,
+	},
+		repository_user.NewSQLStore(db.GetDB()),
+	)
+	connectRedis := cache.IsConnected()
+	fmt.Println("connect redis:", connectRedis)
+	appctx := app_context.NewAppContext(db, pubsublocal.NewPubSub(), cache)
+
+	httpSvr := httpServer.NewServer(appctx)
+	if err = httpSvr.Run(); err != nil {
+		logrus.Fatal(err)
+	}
+
+	// run below code with Grpc
+
+	// go func() {
+	// 	httpSvr := httpServer.NewServer(appctx)
+	// 	if err = httpSvr.Run(); err != nil {
+	// 		logrus.Fatal(err)
+	// 	}
+	// }()
+}
